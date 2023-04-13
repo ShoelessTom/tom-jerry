2583d5fb688daa651bfb60669f1814d6bf64786a
Abrar Zahin <a.zahin1@EDU.epsb.ca>
2023-04-13 13:27:52 -0600

    commit of the school day

---
+++ quiz/js.js
@@ -0,0 +1,12 @@
+document.getElementById("btn").addEventListener("click", btnClicked);
+function btnClicked() {
+    let q1 = document.getElementById("qone").value;
+    let q2 = document.getElementById("qtwo").value;
+    let q3 = document.getElementById("qthree").value;
+    let q4 = document.getElementById("qfour").value;
+    if (q1 = 1941) {
+        let correct = 1
+    } else if {
+        
+    }
+}
